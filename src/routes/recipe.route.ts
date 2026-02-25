130d49f0d39c6f767023ebc9f7a064bc698e9dfb
thaprohit <thaparohit155@gmail.com>
2026-02-25 14:58:49 +0545

    add recipe route

---
+++ src/routes/recipe.route.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { RecipeController } from "../controllers/recipe.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { recipeUpload } from "../config/multer.config";
+
+const router = Router();
+const recipeController = new RecipeController();
+
+router.get("/", recipeController.getAllRecipes.bind(recipeController));
+router.get("/my", authMiddleware, recipeController.getMyRecipes.bind(recipeController));
+router.get("/:id", recipeController.getRecipeById.bind(recipeController));
+router.post("/", authMiddleware, recipeUpload.single("image"), recipeController.createRecipe.bind(recipeController));
+router.patch("/:id", authMiddleware, recipeController.updateRecipe.bind(recipeController));
+router.delete("/:id", authMiddleware, recipeController.deleteRecipe.bind(recipeController));
+
+export default router;
