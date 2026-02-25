b20cfb66de4c2bc21e7aacb4939ff4f5d12d909b
thaprohit <thaparohit155@gmail.com>
2026-02-25 14:56:45 +0545

    update user service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,11 +1,15 @@
 import bcryptjs from "bcryptjs";
 import * as jwt from "jsonwebtoken";
-import { CreateUserDTO, LoginUserDTO } from "../dtos/user.dto";
+import { CreateUserDTO, LoginUserDTO, AdminCreateUserDTO, AdminUpdateUserDTO, UpdateProfileDTO } from "../dtos/user.dto";
 import { UserRepository } from "../repositories/user.repository";
+import { RecipeRepository } from "../repositories/recipe.repository";
+import { FavoriteRepository } from "../repositories/favorite.repository";
 import { HttpError } from "../errors/http-error";
 import { JWT_SECRET, JWT_EXPIRES_IN, BCRYPT_SALT_ROUNDS } from "../config";
 
 const userRepository = new UserRepository();
+const recipeRepository = new RecipeRepository();
+const favoriteRepository = new FavoriteRepository();
 
 export class UserService {
   async createUser(data: CreateUserDTO) {
@@ -58,5 +62,152 @@ export class UserService {
     delete (userObj as any).password;
 
     return { token, user: userObj };
+  }
+
+  // Admin methods
+  async adminCreateUser(data: AdminCreateUserDTO, imagePath?: string) {
+    const emailCheck = await userRepository.getUserByEmail(data.email);
+    if (emailCheck) {
+      throw new HttpError(409, "Email already in use");
+    }
+
+    const usernameCheck = await userRepository.getUserByUsername(data.username);
+    if (usernameCheck) {
+      throw new HttpError(409, "Username already in use");
+    }
+
+    const hashedPassword = await bcryptjs.hash(data.password, BCRYPT_SALT_ROUNDS);
+    
+    const userData: any = {
+      ...data,
+      password: hashedPassword,
+    };
+
+    if (imagePath) {
+      userData.image = imagePath;
+    }
+
+    const newUser = await userRepository.createUser(userData);
+
+    const userObj = (newUser as any).toObject ? (newUser as any).toObject() : newUser;
+    delete (userObj as any).password;
+
+    return userObj;
+  }
+
+  async getAllUsers() {
+    const users = await userRepository.getAllUsers();
+    return users.map(user => {
+      const userObj = (user as any).toObject ? (user as any).toObject() : user;
+      delete (userObj as any).password;
+      return userObj;
+    });
+  }
+
+  async getUserById(id: string) {
+    const user = await userRepository.getUserById(id);
+    if (!user) {
+      throw new HttpError(404, "User not found");
+    }
+
+    const userObj = (user as any).toObject ? (user as any).toObject() : user;
+    delete (userObj as any).password;
+
+    return userObj;
+  }
+
+  async adminUpdateUser(id: string, data: AdminUpdateUserDTO, imagePath?: string) {
+    const user = await userRepository.getUserById(id);
+    if (!user) {
+      throw new HttpError(404, "User not found");
+    }
+
+    if (data.email && data.email !== user.email) {
+      const emailCheck = await userRepository.getUserByEmail(data.email);
+      if (emailCheck) {
+        throw new HttpError(409, "Email already in use");
+      }
+    }
+
+    if (data.username && data.username !== user.username) {
+      const usernameCheck = await userRepository.getUserByUsername(data.username);
+      if (usernameCheck) {
+        throw new HttpError(409, "Username already in use");
+      }
+    }
+
+    const updateData: any = { ...data };
+    if (imagePath) {
+      updateData.image = imagePath;
+    }
+
+    const updatedUser = await userRepository.updateUser(id, updateData);
+    if (!updatedUser) {
+      throw new HttpError(404, "User not found");
+    }
+
+    const userObj = (updatedUser as any).toObject ? (updatedUser as any).toObject() : updatedUser;
+    delete (userObj as any).password;
+
+    return userObj;
+  }
+
+  async updateUserProfile(id: string, data: UpdateProfileDTO, imagePath?: string) {
+    const user = await userRepository.getUserById(id);
+    if (!user) {
+      throw new HttpError(404, "User not found");
+    }
+
+    if (data.email && data.email !== user.email) {
+      const emailCheck = await userRepository.getUserByEmail(data.email);
+      if (emailCheck) {
+        throw new HttpError(409, "Email already in use");
+      }
+    }
+
+    if (data.username && data.username !== user.username) {
+      const usernameCheck = await userRepository.getUserByUsername(data.username);
+      if (usernameCheck) {
+        throw new HttpError(409, "Username already in use");
+      }
+    }
+
+    const updateData: any = { ...data };
+    if (imagePath) {
+      updateData.image = imagePath;
+    }
+
+    const updatedUser = await userRepository.updateUser(id, updateData);
+    if (!updatedUser) {
+      throw new HttpError(404, "User not found");
+    }
+
+    const userObj = (updatedUser as any).toObject ? (updatedUser as any).toObject() : updatedUser;
+    delete (userObj as any).password;
+
+    return userObj;
+  }
+
+  async deleteUser(id: string) {
+    const user = await userRepository.getUserById(id);
+    if (!user) {
+      throw new HttpError(404, "User not found");
+    }
+
+    const userRecipeIds = await recipeRepository.getRecipeIdsByUser(id);
+
+    await Promise.all([
+      favoriteRepository.removeFavoritesByUser(id),
+      favoriteRepository.removeFavoritesByRecipeIds(userRecipeIds),
+    ]);
+
+    await recipeRepository.deleteRecipesByUser(id);
+
+    const deleted = await userRepository.deleteUser(id);
+    if (!deleted) {
+      throw new HttpError(500, "Failed to delete user");
+    }
+
+    return { message: "User deleted successfully" };
   }
 }
