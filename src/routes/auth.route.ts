1de7e09564dddb6caf5558e845da27d97a569d21
thaprohit <thaparohit155@gmail.com>
2026-02-25 14:56:36 +0545

    update auth route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { upload } from "../config/multer.config";
 
 let authController = new AuthController();
 const router = Router();
 
 router.post("/register", authController.register)
 router.post("/login", authController.login)
+router.put("/:id", authMiddleware, upload.single("image"), authController.updateProfile.bind(authController))
+
 export default router;
