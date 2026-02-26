5fc2702fe462ec26321d53f35fe221150b503ebb
thaprohit <thaparohit155@gmail.com>
2026-02-26 15:19:51 +0545

    refactor(recipe-route): update recipe route

--- src/routes/recipe.route.ts
+++ src/routes/recipe.route.ts
@@ -10,7 +10,7 @@ router.get("/", recipeController.getAllRecipes.bind(recipeController));
 router.get("/my", authMiddleware, recipeController.getMyRecipes.bind(recipeController));
 router.get("/:id", recipeController.getRecipeById.bind(recipeController));
 router.post("/", authMiddleware, recipeUpload.single("image"), recipeController.createRecipe.bind(recipeController));
-router.patch("/:id", authMiddleware, recipeController.updateRecipe.bind(recipeController));
+router.patch("/:id", authMiddleware, recipeUpload.single("image"), recipeController.updateRecipe.bind(recipeController));
 router.delete("/:id", authMiddleware, recipeController.deleteRecipe.bind(recipeController));
 
 export default router;
